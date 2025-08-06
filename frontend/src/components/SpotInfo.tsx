eaf60998948cbf8a7460a63b827e7341ff42861a
William Kim <gkim12@students.cs.ubc.ca>
2025-08-06 15:23:15 -0700

    Added spot info section, edit later

--- frontend/src/components/SpotInfo.tsx
+++ frontend/src/components/SpotInfo.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
-import { VStack, Heading, Box, Image, Text } from '@chakra-ui/react'
+import { VStack, Heading, Box, Image, Text, HStack, Center } from '@chakra-ui/react'
 
 const SpotInfo = () => {
   return (
-    <VStack py={16} align="center">
+    <VStack py={20} align="center">
       <Heading size="5xl" fontWeight="bold" fontFamily="Inter" color="#102040">
         What Is SPOT?
       </Heading>
       
-      <Box position="relative" w="fit-content" mx="auto" mt={6} mb={8}>
+      <Box position="relative" w="fit-content" mx="auto" mt={20} mb={8}>
         <Image
           src="/src/assets/disclaimerguy.png"
           alt="Disclaimer"
           width="200px"
           objectFit="contain"
-          mx="auto"   
+          mx="auto"
+          position="absolute"   
+          left="50%"
+          top="-80px"
+          transform="translateX(-50%)"
+          zIndex={2}
         />
         <Box
           px={8}
@@ -35,6 +40,30 @@ const SpotInfo = () => {
           </Text>
         </Box>
       </Box>
+
+      {/* Now time for the small text bubbles */}
+      <HStack gap={20}>
+        <Box bg="#C8DDFA" px={10} py={10} boxShadow="md" borderRadius="xl">
+          <Text fontFamily="Inter" fontSize="xl" fontWeight="light" color="#102040" textAlign="center">
+          SPOT connects students using drugs 
+          <br />
+          with trained volunteers, offering real-
+          <br />
+          time 
+          support and monitoring.
+          </Text>
+        </Box>
+
+        <Box bg="#C8DDFA" px={10} py={10} boxShadow="md" borderRadius="xl">
+          <Text fontFamily="Inter" fontSize="xl" fontWeight="light" color="#102040" textAlign="center"  >
+          Reduce the crucial time period between 
+          <br />
+          an overdose detection and an overdose 
+          <br />
+          response.
+          </Text>
+        </Box>
+      </HStack>
     </VStack>
   )
 }
