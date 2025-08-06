5d5bc3f8e2f74d9c0c0a794e9fc577bed07c15fb
William Kim <gkim12@students.cs.ubc.ca>
2025-08-05 18:11:53 -0700

    Added disclaimer message, need to put guy on top of box

--- frontend/src/components/SpotInfo.tsx
+++ frontend/src/components/SpotInfo.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
-import { Container } from '@chakra-ui/react'
+import { VStack, Heading, Box, Image, Text } from '@chakra-ui/react'
 
 const SpotInfo = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <VStack py={16} align="center">
+      <Heading size="5xl" fontWeight="bold" fontFamily="Inter" color="#102040">
+        What Is SPOT?
+      </Heading>
+      
+      <Box position="relative" w="fit-content" mx="auto" mt={6} mb={8}>
+        <Image
+          src="/src/assets/disclaimerguy.png"
+          alt="Disclaimer"
+          width="200px"
+          objectFit="contain"
+          mx="auto"   
+        />
+        <Box
+          px={8}
+          py={10}
+          bg="white"
+          borderRadius="xl"
+          boxShadow="md"
+          minW={{ base: "90vw", md: "600px" }}
+          border="2px solid"
+          borderColor="blue.100"
+          zIndex={1}
+          position="relative"
+        >
+          <Text fontSize="xl" fontFamily="Inter" color="#102040" textAlign="center" fontWeight="extralight">
+          SPOT is not focused on preventing drug use; we aim to make drug usage a safer 
+          <br />
+          experience and provide callers with a safety net in the case of an overdose.
+          </Text>
+        </Box>
+      </Box>
+    </VStack>
   )
 }
 
